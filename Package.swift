88b6b156e48302d642a050f19b3a35f391b2b4bf
David Steinacher <david.steinacher@bitmovin.com>
2023-09-21 16:47:17 +0200

    update dependencies to the latest version

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "PlayerTesting",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v14), .tvOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -15,9 +15,9 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "BitmovinPlayer", url: "https://github.com/bitmovin/player-ios", from: "3.15.0"),
-        .package(url: "https://github.com/Quick/Nimble", from: "9.0.0"),
-        .package(url: "https://github.com/Quick/Quick", from: "4.0.0"),
+        .package(name: "BitmovinPlayerCore", url: "https://github.com/bitmovin/player-ios-core", from: "3.40.0"),
+        .package(url: "https://github.com/Quick/Nimble", from: "12.0.0"),
+        .package(url: "https://github.com/Quick/Quick", from: "7.0.0"),
         .package(url: "https://github.com/AliSoftware/OHHTTPStubs", from: "9.0.0"),
     ],
     targets: [
@@ -26,7 +26,7 @@ let package = Package(
         .target(
             name: "PlayerTesting",
             dependencies: [
-                "BitmovinPlayer",
+                "BitmovinPlayerCore",
                 "Quick",
                 "Nimble",
                 .product(name: "OHHTTPStubsSwift", package: "OHHTTPStubs"),
